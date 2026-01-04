180524c0733fa98082700476150fabf338dbd41b
Kathan <kathanmajithia@gmail.com>
2026-01-04 21:23:28 +0530

    Post method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI(docs_url="/docs", redoc_url="/redoc")
 
@@ -21,4 +22,14 @@ def blob(limit=10,lod : bool = True,sort: Optional[str] = None):
 @app.get('/blob/{id}')
 def show(id: int):
     return {"data":id}
+
+class Blob(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+    
+
+@app.post('/blob')
+def create(req: Blob):
+    return {'data':f"Blob is created with title as {req.title}"}
 
