9a9a4197fbd7fb3664da3d7ce530040f5b1164d9
Kathan <kathanmajithia@gmail.com>
2026-01-04 22:17:18 +0530

    Insert

--- blog/database.py
+++ blog/database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 SQLALCHEMY_DB_URL = 'sqlite:///./blog.db'
 
 eng = create_engine(SQLALCHEMY_DB_URL,connect_args={"check_same_thread":False})
 
-Session = sessionmaker(bind=eng,autocommit=False,autoflush=False)
+SessionLocal = sessionmaker(bind=eng,autocommit=False,autoflush=False)
 
 Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import eng
+from .database import eng,SessionLocal
+from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(eng)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(req: schemas.Blog):
+def create(req: schemas.Blog,db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=req.title,body=req.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
     return req
