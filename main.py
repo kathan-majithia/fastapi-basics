e36582f279d565aa6e348f96e74df170898a9245
Kathan <kathanmajithia@gmail.com>
2026-01-04 19:03:33 +0530

    Query Parameter

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url="/docs", redoc_url="/redoc")
 
-@app.get('/')
+@app.get("/")
 def index():
-    return 'Hello'
+    return {"data":"Hello"}
 
 @app.get('/about')
 def about():
     return {"name":"Kathan Majithia"}
 
+@app.get('/blob')
+def blob(limit=10,lod : bool = True):
+    if lod:
+        return {"data" : f"{limit} blobs from the db"}
+    else:
+        return {"data":"Not loaded"}
+
 @app.get('/blob/{id}')
-def show(id):
+def show(id: int):
     return {"data":id}
 
