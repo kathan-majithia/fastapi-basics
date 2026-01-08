28ef31d16ce2bfd7b8368b7715e89310da5b9cb8
Kathan <kathanmajithia@gmail.com>
2026-01-08 11:30:32 +0530

    JWT Access Token

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter,Depends, HTTPException, status
-from .. import schemas, database, models
+from .. import schemas, database, models, token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
-
 router = APIRouter(prefix='/login', tags=['Authentication'])
 
 @router.post('/')
@@ -14,4 +13,5 @@ def user_login(req: schemas.Login, db: Session = Depends(database.get_db)):
     if not Hash.verify(user.password,req.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Password invalid")
         
-    return user
+    access_token = token.create_access_token(data={"sub":user.email})
+    return {"access_token":access_token,"token_type":"bearer"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -51,3 +51,10 @@ class User(UserBase):
 class Login(BaseModel):
     username: str
     password: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: Optional[str] = None
