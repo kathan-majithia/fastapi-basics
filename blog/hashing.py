674c222264f7f31707fad89733f473a56a62eb3c
Kathan <kathanmajithia@gmail.com>
2026-01-08 10:53:32 +0530

    Password authentication

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,3 +5,6 @@ cc = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def bcrypt(password: str):
         return cc.hash(password)
+    
+    def verify(cipher_text, plain_text):
+        return cc.verify(plain_text,cipher_text)
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter,Depends, HTTPException, status
 from .. import schemas, database, models
 from sqlalchemy.orm import Session
+from ..hashing import Hash
 
 router = APIRouter(prefix='/login', tags=['Authentication'])
 
@@ -9,4 +10,8 @@ def user_login(req: schemas.Login, db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == req.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User email incorrect")
+    
+    if not Hash.verify(user.password,req.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Password invalid")
+        
     return user
